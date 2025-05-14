3f068b447eed3a2e9a62b60448ba3d4c83774b2f
vikasvtt <vikastirakannanavar@gmail.com>
2025-05-14 10:21:42 +0530

    savess

---
+++ third.java
@@ -0,0 +1,12 @@
+public class third{
+
+    public void printQuote() {
+        System.out.println("Success doesn't come from what you do occasionally, it comes from what you do consistently.");
+    }
+
+    public static void main(String[] args) {
+        third obj = new third();
+
+        obj.printQuote();
+    }
+}
